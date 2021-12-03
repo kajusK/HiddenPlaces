50f2e8234761359b3e403abab161f743859b12e4
Jakub Kaderka <jakub.kaderka@gmail.com>
2021-12-03 17:39:49 +0100

    Moved pylint configuration to setup.cfg

--- app/utils/enums.py
+++ app/utils/enums.py
@@ -32,7 +32,7 @@ class StringEnum(OrderedEnum):
     Define items like:
     FOO = 0, _("Translated string")
     """
-    translation: str
+    translation: str # pylint: disable=invalid-name
 
     def __new__(cls, value: int, translation: str):
         """Custom object creation.
