d606e1ad9b9a91247eac92f38868bb9aa6c7e39e
Jakub Kaderka <jakub.kaderka@gmail.com>
2022-01-18 16:56:34 +0100

    Added select from map to location and POI

--- app/static/js/map.js
+++ app/static/js/map.js
@@ -1,38 +1,105 @@
-var map = L.map('map').setView([49.8, 15.5], 8);
+class MyMap {
+    /**
+     * Creates leaflet map tied to given div
+     *
+     * @param {str} divId   ID of the div to draw map to
+     */
+    constructor(divId) {
+        this.map = L.map(divId).setView([49.8, 15.5], 8);
 
-L.tileLayer('https://mapserver.mapy.cz/turist-m/{z}-{x}-{y}', {
-	attribution: '&copy; <a href="https://www.mapy.cz">Mapy.cz</a>',
-	minZoom: 7,
-	maxZoom: 19
-}).addTo(map);
+        L.tileLayer('https://mapserver.mapy.cz/turist-m/{z}-{x}-{y}', {
+            attribution: '&copy; <a href="https://www.mapy.cz">Mapy.cz</a>',
+            minZoom: 3,
+            maxZoom: 19
+        }).addTo(this.map);
 
-scale = L.control.scale({
-	imperial: false,
-    maxWidth: 200
-}).addTo(map);
+        L.control.scale({
+            imperial: false,
+            maxWidth: 200
+        }).addTo(this.map);
 
-var markers = L.markerClusterGroup();
+        this.markers = L.markerClusterGroup();
+        this.map.addLayer(this.markers);
+    }
 
-let request = new XMLHttpRequest();
-request.onload = function() {
-    let data = JSON.parse(this.responseText);
-    data.locations.forEach(location => {
-        let marker = L.marker([location.latitude, location.longitude], {title: location.name});
-        marker.bindPopup(`
-            <div style="min-width: 200px">
-                <img src="${location.image}" style="width: 100%">
-                <h4 class="mt-2">
-                    <a href="/location/${location.id}">${location.name}</a>
-                </h4>
-                <p class="overflow-hidden" style="display: -webkit-box; -webkit-box-orient: vertical; -webkit-line-clamp: 4;">
-                    ${location.description}
-                </p>
-            </div>
-        `);
-        markers.addLayer(marker);
-    })
-    map.addLayer(markers);
+    /**
+     * Force map redrawing when parent container dimensions changed
+     */
+    redraw() {
+        this.map.invalidateSize()
+    }
+
+    /**
+     * Fetches data from JSON endpoint
+     *
+     * @param {str} url    URL of the endpoint to fetch data from
+     */
+    fetchLocations(url) {
+        let request = new XMLHttpRequest();
+        let markers = this.markers
+
+        request.onload = function() {
+            let data = JSON.parse(this.responseText);
+            data.locations.forEach(location => {
+                let marker = L.marker([location.latitude, location.longitude], {title: location.name});
+                marker.bindPopup(`
+                    <div style="min-width: 200px">
+                        <img src="${location.image}" style="width: 100%">
+                        <h4 class="mt-2">
+                            <a href="/location/${location.id}">${location.name}</a>
+                        </h4>
+                        <p class="overflow-hidden" style="display: -webkit-box; -webkit-box-orient: vertical; -webkit-line-clamp: 4;">
+                            ${location.description}
+                        </p>
+                    </div>
+                `);
+                markers.addLayer(marker);
+            })
+        }
+
+        request.open("GET", url)
+        request.send();
+    }
+
+    /**
+     * Shows current user location (must enable localization in browser)
+     */
+    showLocation(zoom=false) {
+        /* TODO set icon */
+        this.map.on('locationfound', (e) => {
+            let radius = e.accuracy
+
+            L.marker(e.latlng).addTo(this.map)
+            L.circle(e.latlng, radius).addTo(this.map)
+        })
+        this.map.locate({setView: zoom, maxZoom: 14});
+    }
+
+    /**
+     * Adds marker to given position
+     *
+     * @param {latlng} latlng       Position of marker
+     * @param {str} title           Title of the marker
+     */
+    addMarker(latlng, title="") {
+        L.marker(latlng, {title: title}).addTo(this.markers)
+    }
+
+    /**
+     * Zoom to the given position on map
+     *
+     * @param {latlng} latlng       Position to zoom to
+     */
+    zoomPosition(latlng) {
+        this.map.setView(latlng, 14)
+    }
+
+    /**
+     * Removes all markers
+     */
+    clearMarkers() {
+        this.map.removeLayer(this.markers);
+        this.markers = L.markerClusterGroup();
+        this.map.addLayer(this.markers);
+    }
 }
-
-request.open("GET", map_api_url)
-request.send();
